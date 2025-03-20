a0855b6ae0c5479e75099ae30b4c721a4c09346f
ifs21011-itdel <archicosemb@gmail.com>
2025-03-20 08:02:13 +0700

    7000 -> 7700

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(
   cors({
-    origin: ["http://194.163.40.203:7000"],
+    origin: ["http://194.163.40.203:7700"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
